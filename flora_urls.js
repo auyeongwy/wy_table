dda132550efc7669bbccfff14e81bf8d8f626485
Au Yeong Wing Yau <auyeongwy@localhost.localdomain>
2017-06-12 23:23:07 +0800

    Initial commit.

---
+++ flora_urls.js
@@ -0,0 +1,27 @@
+var image_links = [
+'<a href="https://www.flickr.com/photos/auywy/34972484832/in/dateposted-public/" title="Shining bright in the gloom"><img src="https://c1.staticflickr.com/5/4215/34972484832_60f46c8435_n.jpg" width="320" height="213" alt="Shining bright in the gloom"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34307053053/in/dateposted-public/" title="Ready To Blossom"><img src="https://c1.staticflickr.com/5/4245/34307053053_a848d012eb_n.jpg" width="320" height="213" alt="Ready To Blossom"></a>',
+
+'<a  href="https://www.flickr.com/photos/auywy/34307053203/in/dateposted-public/" title="Dreamy Osteospermum"><img src="https://c1.staticflickr.com/5/4214/34307053203_2f4aa1a26d_n.jpg" width="320" height="213" alt="Dreamy Osteospermum"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34272759524/in/dateposted-public/" title="Rudbeckia Beginnings"><img src="https://c1.staticflickr.com/5/4227/34272759524_677e1c2ca0_n.jpg" width="320" height="213" alt="Rudbeckia Beginnings"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34307052943/in/dateposted-public/" title="Happy Glorious Yellow"><img src="https://c1.staticflickr.com/5/4257/34307052943_c0586b60ca_n.jpg" width="320" height="213" alt="Happy Glorious Yellow"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34228575904/in/dateposted-public/" title="Coloured succulent growing"><img src="https://c1.staticflickr.com/5/4216/34228575904_221c12d12f_n.jpg" width="320" height="213" alt="Coloured succulent growing"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34900663981/in/dateposted-public/" title="Vivid Budding Red"><img src="https://c1.staticflickr.com/5/4249/34900663981_681b221399_n.jpg" width="320" height="213" alt="Vivid Budding Red"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34900663791/in/dateposted-public/" title="Explosion Of Youth"><img src="https://c1.staticflickr.com/5/4227/34900663791_3f138e804c_n.jpg" width="320" height="213" alt="Explosion Of Youth"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34219003183/in/dateposted-public/" title="Tiny Blossoms"><img src="https://c1.staticflickr.com/5/4222/34219003183_068ccbb75f_n.jpg" width="320" height="213" alt="Tiny Blossoms"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34219002753/in/dateposted-public/" title="Little vain magenta"><img src="https://c1.staticflickr.com/5/4272/34219002753_babcaf47c6_n.jpg" width="320" height="213" alt="Little vain magenta"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34219002963/in/dateposted-public/" title="Colourful Aloe"><img src="https://c1.staticflickr.com/5/4243/34219002963_937202e314_n.jpg" width="213" height="320" alt="Colourful Aloe"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34219003363/in/dateposted-public/" title="Plum blossom on spring day"><img src="https://c1.staticflickr.com/5/4203/34219003363_c6b3e25294_n.jpg" width="213" height="320" alt="Plum blossom on spring day"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34219002603/in/dateposted-public/" title="Vivid blushing beauties"><img src="https://c1.staticflickr.com/5/4227/34219002603_a3629d4849_n.jpg" width="213" height="320" alt="Vivid blushing beauties"></a>'
+];
