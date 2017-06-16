5318097c64d8aba009d64682bab9804d7f9f9901
Au Yeong Wing Yau <auyeongwy@localhost.localdomain>
2017-06-16 21:33:01 +0800

    Added street_urls.js

---
+++ street_urls.js
@@ -0,0 +1,7 @@
+var image_links = [
+'<a href="https://www.flickr.com/photos/auywy/35052721686/in/dateposted-public/" title="Singapore 2017: Window Cleaners Working On A Hotel"><img src="https://c1.staticflickr.com/5/4243/35052721686_745259aab5_n.jpg" width="213" height="320" alt="Singapore 2017: Window Cleaners Working On A Hotel"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34347952683/in/dateposted-public/" title="Singapore 2017: People Among Urban Geometry"><img src="https://c1.staticflickr.com/5/4282/34347952683_b0c9d34f26_n.jpg" width="213" height="320" alt="Singapore 2017: People Among Urban Geometry">',
+
+'<a href="https://www.flickr.com/photos/auywy/18558379529/in/album-72157648249054783/" title="Friends Hugging And Putting On A Necklace"><img class="aligncenter size-full" src="https://c1.staticflickr.com/1/457/18558379529_2b89d6cca5_n.jpg" width="320" height="213" alt="Friends Hugging And Putting On A Necklace"></a>'
+];
