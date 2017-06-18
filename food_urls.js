9a8181adccfc068b701d1aa34491e8b2929e1136
Au Yeong Wing Yau <auyeongwy@localhost.localdomain>
2017-06-19 04:51:09 +0800

    Added food_urls.js

---
+++ food_urls.js
@@ -0,0 +1,5 @@
+var image_links = [
+'<a href="https://www.flickr.com/photos/auywy/35294221276/in/album-72157650715044112/" title="Caviar On A Plate"><img src="https://c1.staticflickr.com/5/4214/35294221276_77985e46ac_n.jpg" width="320" height="213" alt="Caviar On A Plate"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/34544103604/in/album-72157672126888706/" title="Golden French Fries Close-up"><img src="https://c1.staticflickr.com/5/4196/34544103604_754dc41032_n.jpg" width="320" height="213" alt="Golden French Fries Close-up"></a>'
+];
