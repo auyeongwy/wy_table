d494edf89a7f682219ff9928adcfc1a7ec5dd439
Au Yeong Wing Yau <auyeongwy@localhost.localdomain>
2017-07-08 22:43:51 +0800

    add architecture and modified flora

---
+++ architecture_urls.js
@@ -0,0 +1,11 @@
+var image_links = [
+'<a href="https://www.flickr.com/photos/auywy/35414453561/in/album-72157649995470440/" title="Singapore 2017: The Shoppes Marina Bay Sands  Water Fountain Feature"><img src="https://farm5.staticflickr.com/4210/35414453561_0688c54ed9_n.jpg" width="320" height="213" alt="Singapore 2017: The Shoppes Marina Bay Sands  Water Fountain Feature"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/33744479621/in/album-72157649995470440/" title="Paris 2017: Petit Palais Ceiling And Floor Design"><img src="https://farm4.staticflickr.com/3894/33744479621_87ac339b4e_n.jpg" width="214" height="320" alt="Paris 2017: Petit Palais Ceiling And Floor Design"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/29115385483/in/album-72157649995470440/" title="Tokyo 2016: Hotel Villa Fontaine Tamachi 1"><img src="https://farm9.staticflickr.com/8672/29115385483_3b6f13a675_n.jpg" width="320" height="213" alt="Tokyo 2016: Hotel Villa Fontaine Tamachi 1"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/29728581926/in/album-72157649995470440/" title="Tokyo 2016: Hotel Villa Fontaine 3"><img src="https://farm9.staticflickr.com/8362/29728581926_616bfeba92_n.jpg" width="320" height="215" alt="Tokyo 2016: Hotel Villa Fontaine 3"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/29562959670/in/album-72157649995470440/" title="Coloured Ceiling Skylight"><img src="https://farm9.staticflickr.com/8817/29562959670_7d4f31bbf8_n.jpg" width="320" height="213" alt="Coloured Ceiling Skylight"></a>'
+];
