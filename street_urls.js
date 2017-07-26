f0c3907c4b31ce95ed9043be43b362979b61ccdc
Au Yeong Wing Yau <auyeongwy@localhost.localdomain>
2017-07-26 19:44:21 +0800

    Updated street photographs from 2013.

--- street_urls.js
+++ street_urls.js
@@ -3,5 +3,17 @@ var image_links = [
 
 '<a href="https://www.flickr.com/photos/auywy/34347952683/in/dateposted-public/" title="Singapore 2017: People Among Urban Geometry"><img src="https://c1.staticflickr.com/5/4282/34347952683_b0c9d34f26_n.jpg" width="213" height="320" alt="Singapore 2017: People Among Urban Geometry">',
 
-'<a href="https://www.flickr.com/photos/auywy/18558379529/in/album-72157648249054783/" title="Friends Hugging And Putting On A Necklace"><img class="aligncenter size-full" src="https://c1.staticflickr.com/1/457/18558379529_2b89d6cca5_n.jpg" width="320" height="213" alt="Friends Hugging And Putting On A Necklace"></a>'
+'<a href="https://www.flickr.com/photos/auywy/18558379529/in/album-72157648249054783/" title="Friends Hugging And Putting On A Necklace"><img class="aligncenter size-full" src="https://c1.staticflickr.com/1/457/18558379529_2b89d6cca5_n.jpg" width="320" height="213" alt="Friends Hugging And Putting On A Necklace"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/10067004585/in/photolist-VZ1n7Q-TDbMzn-RmMvoi-NTea6g-Mz7LDm-CWKFif-CHs7rw-woSfmh-uerE4P-rv7SMJ-q49Xdu-quxvvJ-pgVU1q-penTvB-mVSBvg-ozvNYz-mG3PrU-j4rTSv-irMABf-iw4p4Z-jegTw1-iCgXNe-gkA2Jz-g5AdqK-fNutGv-fBG4Pb-fyrixm-eBDXwN-eBDaRE-eyx4my" title="Quiet Foggy Urban City"><img src="https://farm4.staticflickr.com/3817/10067004585_815db2415d_n.jpg" width="320" height="213" alt="Quiet Foggy Urban City"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/9598246055/in/photolist-fCaw9X" title="Surveyor With Theodolite On Urban Road"><img src="https://farm6.staticflickr.com/5485/9598246055_17c8b38108_n.jpg" width="320" height="213" alt="Surveyor With Theodolite On Urban Road"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/9773175092/in/photolist-PPE87q-NgSVfW-HjnU44-w42JnJ-reZQ2M-rDCVnc-qu1C4a-qtvJUF-phJSwa-pzExPW-mVSBvg-fTC5sQ-ft6efJ" title="Man Sleeping In The Crowd In Chinatown"><img src="https://farm3.staticflickr.com/2841/9773175092_4c8fd27dcc_n.jpg" width="320" height="213" alt="Man Sleeping In The Crowd In Chinatown"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/10219521404/in/photolist-VpumWs-gz4HEU" title="Window Cleaners Cleaning Skyscraper"><img src="https://farm4.staticflickr.com/3812/10219521404_f03b187bae_n.jpg" width="213" height="320" alt="Window Cleaners Cleaning Skyscraper"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/36090687486/in/dateposted-public/" title="Night rider"><img src="https://farm5.staticflickr.com/4306/36090687486_9f3cebbe73_n.jpg" width="320" height="213" alt="Night rider"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/10798859943/in/photolist-WuBC5B-WtdgqN-WfREAu-Wwwx2t-Wo7geX-S3a4tg-Sau9Y4-S2WYTQ-RRYjSW-S4BnqS-LbWt2C-KSQB8W-KvuZF1-KjQi6X-H7uDT9-E2qQfK-DPmUfp-CWKFif-DwWo6S-D2K5Ho-w3G7EK-oupk4v-ovo4Gc-oHLu6a-oDBgJb-oEqEqQ-o5pT22-na8rQ4-kbtwyy-irMABf-iw4p4Z-jegTw1-hsfYQp-fDsrgB-eBD89w-eBCikh-eytUwP" title="Scrutinize the tiny"><img src="https://farm8.staticflickr.com/7309/10798859943_99f6c442b1_n.jpg" width="320" height="213" alt="Scrutinize the tiny"></a>',
 ];
