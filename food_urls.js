daac64fc2ee8722ed0c771862e576bb8c1a4c948
Au Yeong Wing Yau <auyeongwy@localhost.localdomain>
2017-07-29 13:23:13 +0800

    Added to food gallery.

--- food_urls.js
+++ food_urls.js
@@ -1,4 +1,10 @@
 var image_links = [
+'<a href="https://www.flickr.com/photos/auywy/36116573076/in/album-72157672126888706/" title="Octopus Sushi With Mayonnaise"><img src="https://farm5.staticflickr.com/4303/36116573076_3bb9fa6000_n.jpg" width="320" height="213" alt="Octopus Sushi With Mayonnaise"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/35696934550/in/album-72157672126888706/" title="Healthy Dark Green Vegetables"><img src="https://farm5.staticflickr.com/4321/35696934550_36a999f3e7_n.jpg" width="320" height="213" alt="Healthy Dark Green Vegetables"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/35283050151/in/album-72157672126888706/" title="Black And White Seseme Seeds"><img src="https://farm5.staticflickr.com/4224/35283050151_775f1346af_n.jpg" width="320" height="213" alt="Black And White Seseme Seeds"></a>',
+
 '<a href="https://www.flickr.com/photos/auywy/35294221276/in/album-72157650715044112/" title="Caviar On A Plate"><img src="https://c1.staticflickr.com/5/4214/35294221276_77985e46ac_n.jpg" width="320" height="213" alt="Caviar On A Plate"></a>',
 
 '<a href="https://www.flickr.com/photos/auywy/34544103604/in/album-72157672126888706/" title="Golden French Fries Close-up"><img src="https://c1.staticflickr.com/5/4196/34544103604_754dc41032_n.jpg" width="320" height="213" alt="Golden French Fries Close-up"></a>'
