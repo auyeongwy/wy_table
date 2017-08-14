936e7b2be0d709379774f1c5a570936605ded6fe
Au Yeong Wing Yau <auyeongwy@localhost.localdomain>
2017-08-14 19:04:12 +0800

    Added Still Life page

---
+++ still_life_urls.js
@@ -0,0 +1,3 @@
+var image_links = [
+'<a href="https://www.flickr.com/photos/auywy/36424086431/in/dateposted-public/" title="Cool Blue Pool Water In Hot Sunny Afternoon"><img src="https://farm5.staticflickr.com/4333/36424086431_e766443e22_n.jpg" width="320" height="213" alt="Cool Blue Pool Water In Hot Sunny Afternoon"></a>'
+];
