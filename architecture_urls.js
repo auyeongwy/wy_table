cb123de949c4ad026a489f68d4bb2d4a492fe8bb
Au Yeong Wing Yau <auyeongwy@localhost.localdomain>
2017-08-27 22:20:24 +0800

    Updated architeture gallery

--- architecture_urls.js
+++ architecture_urls.js
@@ -1,4 +1,6 @@
 var image_links = [
+'<a href="https://www.flickr.com/photos/auywy/29910819782/in/photolist-Mz7LDm-Myh2ng-LGyaw3" title="Tokyo 2016: Tokyo Tower Details 1"><img src="https://farm6.staticflickr.com/5246/29910819782_9cbc9edea9_n.jpg" width="320" height="213" alt="Tokyo 2016: Tokyo Tower Details 1"></a>',
+
 '<a href="https://www.flickr.com/photos/auywy/36446404041/in/dateposted-public/" title="Singapore 2017: Victoria Concert Hall Interior 5"><img src="https://farm5.staticflickr.com/4436/36446404041_c26ace980c_n.jpg" width="213" height="320" alt="Singapore 2017: Victoria Concert Hall Interior 5"></a>',
 
 '<a href="https://www.flickr.com/photos/auywy/35414453561/in/album-72157649995470440/" title="Singapore 2017: The Shoppes Marina Bay Sands  Water Fountain Feature"><img src="https://farm5.staticflickr.com/4210/35414453561_0688c54ed9_n.jpg" width="320" height="213" alt="Singapore 2017: The Shoppes Marina Bay Sands  Water Fountain Feature"></a>',
