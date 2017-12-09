e9542a56e412468279d008f86bb9a2f887502985
Au Yeong Wing Yau <auyeongwy@gmail.com>
2017-12-10 00:14:54 +0800

    Added to architecture gallery.

--- architecture_urls.js
+++ architecture_urls.js
@@ -13,5 +13,43 @@ var image_links = [
 
 '<a href="https://www.flickr.com/photos/auywy/29728581926/in/album-72157649995470440/" title="Tokyo 2016: Hotel Villa Fontaine 3"><img src="https://farm9.staticflickr.com/8362/29728581926_616bfeba92_n.jpg" width="320" height="215" alt="Tokyo 2016: Hotel Villa Fontaine 3"></a>',
 
-'<a href="https://www.flickr.com/photos/auywy/29562959670/in/album-72157649995470440/" title="Coloured Ceiling Skylight"><img src="https://farm9.staticflickr.com/8817/29562959670_7d4f31bbf8_n.jpg" width="320" height="213" alt="Coloured Ceiling Skylight"></a>'
+'<a href="https://www.flickr.com/photos/auywy/30411348240/in/photolist-Nkm7jm-MSeDZ2-nRxh8c-nQAVDF-nLBHqj-nZCX5T-qG1gDt-nD7XXC-nUKxJQ-sdc1ZJ-nujvaD-nSEFDN-nWPZaY-nJ3fZG-nxDHiF-nS2AN9" title="Singapore 2016: Orchard Central Layers"><img src="https://farm6.staticflickr.com/5328/30411348240_b6ce9b45f6_n.jpg" width="214" height="320" alt="Singapore 2016: Orchard Central Layers"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/31411819485/in/photolist-PRKMyz-P7ksCL-Nkm7jm-BJZkdn-BPVEAA-CyRk5P-BH9wCV-tZ7pYe-uJ4Co1-uncSkL-q3qpkQ-qVWQnf-qfPt2w-qRdnqs-pC5Cyx-pqWbAw-p5qQXP-t4EUbf-owpEvL-nQAVDF" title="Singapore 2016: Dhoby Gaut Underground Train Station"><img src="https://farm6.staticflickr.com/5655/31411819485_8130b2ee25_n.jpg" width="320" height="214" alt="Singapore 2016: Dhoby Gaut Underground Train Station"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/29562959670/in/album-72157649995470440/" title="Coloured Ceiling Skylight"><img src="https://farm9.staticflickr.com/8817/29562959670_7d4f31bbf8_n.jpg" width="320" height="213" alt="Coloured Ceiling Skylight"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/24957718695/in/photolist-LwiHrB-E2qQfK-DPmUfp-CWKFif-DwWo6S-DLPMWk-D2K5Ho-Cu9eJv-ux3NHQ" title="Singapore National Gallery: Overlooking The Rotunda 4"><img src="https://farm2.staticflickr.com/1579/24957718695_e5c7bb85cb_n.jpg" width="320" height="214" alt="Singapore National Gallery: Overlooking The Rotunda 4"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/23458965729/in/photolist-BnTTf1-BJZkdn-BPVEAA-AoZa87" title="Urban Geometry Nov 2015"><img src="https://farm6.staticflickr.com/5652/23458965729_a177d1dec3_n.jpg" width="214" height="320" alt="Urban Geometry Nov 2015"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/24792451035/in/photolist-LwiHrB-E2qQfK-DPmUfp-CWKFif-DwWo6S-DLPMWk-D2K5Ho-Cu9eJv-ux3NHQ" title="Singapore National Gallery: Interior Courtyard 2"><img src="https://farm2.staticflickr.com/1622/24792451035_3cd7324307_n.jpg" width="320" height="214" alt="Singapore National Gallery: Interior Courtyard 2"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/23639465141/in/photolist-C1Wrpc-BEaXhh-y8PH2P-tkRWHn-iw4p4Z-jegTw1" title="Singapore Flyer 3"><img src="https://farm1.staticflickr.com/667/23639465141_0effc82a1d_n.jpg" width="320" height="214" alt="Singapore Flyer 3"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/23514832966/in/photolist-BnTTf1-BJZkdn-BPVEAA-AoZa87" title="Descending Escalator In X Shape"><img src="https://farm1.staticflickr.com/772/23514832966_b6b0cd0c16_n.jpg" width="320" height="214" alt="Descending Escalator In X Shape"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/22387211400/in/photolist-LGyaw3-A7hiZs-qebDqF" title="Red Corner And Stairs"><img src="https://farm1.staticflickr.com/748/22387211400_8d6a6b7f21_n.jpg" width="214" height="320" alt="Red Corner And Stairs"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/22576245096/in/photolist-BnTTf1-BJZkdn-BPVEAA-AoZa87" title="Suntec City Atrium"><img src="https://farm6.staticflickr.com/5641/22576245096_0a4d158ac7_n.jpg" width="214" height="320" alt="Suntec City Atrium"></a>',
+
+'<a  href="https://www.flickr.com/photos/auywy/18754987314/in/photolist-uzjdjf" title="Singapore Power Building: Modern Brutalism 1"><img src="https://farm1.staticflickr.com/329/18754987314_1cb4129841_n.jpg" width="320" height="214" alt="Singapore Power Building: Modern Brutalism 1"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/14069880921/in/photolist-qHxBm9-qfPt2w-nvhm3C-rrPqy8-ndEdrK-nqb4Hf-nGnARB-nbqh59-stxUaC-sGsbxD-nriPpP" title="ION night lights"><img src="https://farm6.staticflickr.com/5198/14069880921_70d821e2b9_n.jpg" width="214" height="320" alt="ION night lights"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/14615786693/in/photolist-ojK26j-odfgft-ocjrmK-ogxJbi-znstdA-oefaMv-orAWED-onNRzZ-okutif-o5pT22-pL68Yg-o1fgNj-t1bPEB-toa85e-tMuhVr-nSySH9" title="Dongdaemun Design Plaza 6"><img src="https://farm3.staticflickr.com/2912/14615786693_51cc085c70_n.jpg" width="320" height="214" alt="Dongdaemun Design Plaza 6"></a>',
+
+'<a  href="https://www.flickr.com/photos/auywy/17972375449/in/photolist-ojK26j-odfgft-ocjrmK-ogxJbi-znstdA-oefaMv-orAWED-onNRzZ-okutif-o5pT22-pL68Yg-o1fgNj-t1bPEB-toa85e-tMuhVr-nSySH9" title="Dongdaemun Design Plaza 2"><img src="https://farm9.staticflickr.com/8804/17972375449_d3d744efce_n.jpg" width="320" height="214" alt="Dongdaemun Design Plaza 2"></a>',
+
+'<a  href="https://www.flickr.com/photos/auywy/14729579001/in/photolist-orAWED" title="Dongdaemun History &amp; Culture Park Interior 8: Resting Gallery"><img src="https://farm6.staticflickr.com/5587/14729579001_dc27268df5_n.jpg" width="320" height="214" alt="Dongdaemun History &amp; Culture Park Interior 8: Resting Gallery"></a>',
+
+'<a  href="https://www.flickr.com/photos/auywy/15346681681/in/photolist-LGyaw3-po8KN2-kGGFK4-TM7yz2-knDeB2-i9duao-hudqEY" title="Colourful Vivid Vintage Urban Architecture"><img src="https://farm3.staticflickr.com/2948/15346681681_13649c450e_n.jpg" width="320" height="214" alt="Colourful Vivid Vintage Urban Architecture"></a>',
+
+'<a  href="https://www.flickr.com/photos/auywy/15276468992/in/photolist-pqYrzn-pe2CFY-p5fUk2-pgVU1q-oXL5o3" title="Lasalle College Of The Arts 3"><img src="https://farm4.staticflickr.com/3855/15276468992_f8e8c58683_n.jpg" width="214" height="320" alt="Lasalle College Of The Arts 3"></a>',
+
+'<a  href="https://www.flickr.com/photos/auywy/15937466865/in/photolist-RmMvoi-zzg2RH-wLSiH6-zDFfUL-wBbg67-qixPCG-qhkFDt-qaiDPj-pSmpLc-q87nSX-q5yQAj-pVtCxq-eBzat2-fyrixm" title="Singapore National Stadium 5"><img src="https://farm8.staticflickr.com/7531/15937466865_7f032915d4_n.jpg" width="320" height="213" alt="Singapore National Stadium 5"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/15951151572/in/photolist-RmMvoi-zzg2RH-wLSiH6-zDFfUL-wBbg67-qixPCG-qhkFDt-qaiDPj-pSmpLc-q87nSX-q5yQAj-pVtCxq-eBzat2-fyrixm" title="Singapore Indoor Stadium 2"><img src="https://farm8.staticflickr.com/7501/15951151572_6b34f262d4_n.jpg" width="320" height="214" alt="Singapore Indoor Stadium 2"></a>',
+
+'<a href="https://www.flickr.com/photos/auywy/15964770406/in/photolist-qjKC3b" title="Cathay Building Facade 2"><img src="https://farm9.staticflickr.com/8621/15964770406_f5ec10036f_n.jpg" width="320" height="214" alt="Cathay Building Facade 2"></a>'
 ];
